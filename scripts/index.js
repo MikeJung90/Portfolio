605db552dde8ce46c88ea38e11dba5787e8726f5
MikeJung90 <kpnba90@gmail.com>
2019-04-11 15:13:42 -0400

    started on html and css

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+'use strict';
+/* global $ */
+
+// code to scoll to a section in you website
+
+// $('#nav').find('.nav-link-skills').click(function(event) {
+//   event.preventDefault();
+//   const section = $(this).attr('href');
+//   $('body').animate({
+//     scrollTop: $(section).offset().top
+//   });
+// });
