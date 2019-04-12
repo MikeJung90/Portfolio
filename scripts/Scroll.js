20c49ddf0766719241d6a272a327fbb98ec16b69
MikeJung90 <kpnba90@gmail.com>
2019-04-12 13:28:47 -0400

    add images and scrolling, has issues still

---
+++ scripts/Scroll.js
@@ -0,0 +1,21 @@
+'use strict';
+/* global $ */
+
+// code to scoll to a section in you website
+const scrollTo = (function() {
+  function scrollToHome() {
+    $('nav').find('a').click(function(event) { // select class that triggers scoll
+      event.preventDefault();
+      const section = $(this).attr('href');
+      $('html, body').animate({ // class you want to scroll to
+        scrollTop: $(section).offset().top
+      }, 100); //animation time length
+    });
+  }
+
+  function bindScroll() {
+    scrollToHome();
+  }
+
+  return bindScroll;
+}());
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,7 @@
 'use strict';
-/* global $ */
+/* global $, Scroll */
 
-// code to scoll to a section in you website
 
-// $('#nav').find('.nav-link-skills').click(function(event) {
-//   event.preventDefault();
-//   const section = $(this).attr('href');
-//   $('body').animate({
-//     scrollTop: $(section).offset().top
-//   });
-// });
+$(document).ready(function(){
+  Scroll.bindScroll();
+});
